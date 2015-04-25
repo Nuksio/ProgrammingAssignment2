7d5e449ba066be39f5124032fa2e84fc92d6dab5
Nuksio <petri@halla-aho.org>
2015-04-25 14:48:12 +0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,16 +3,16 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
+makeCacheMatrix <- function(x = matrix()) {  ##function makeCacheMatrix created and input will be x which will be matrix
+  m <- NULL ##preparing value m with value NULL in a global environment
+  set <- function(y) { ##creating function set with input y
+    x <<- y ##variable x in function with value y, not in global environment 
+    m <<- NULL ##variable m in function with value NULL, not in global environment
   }
-  get <- function() x
+  get <- function() x ##creates function get() with input x
   setmx <- function(solve) m <<- solve
-  getmx <- function() m
-  list(set = set, get = get,
+  getmx <- function() m  ##creates function getmx() with input m
+  mxlist(set = set, get = get,
        setmx = setmx,
        getmx = getmx)
 }
